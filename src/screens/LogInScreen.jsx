bffed4f1ecee0fd1ca6d0f8700f8505590f22ffd
tetsu <cda07c5117@gmail.com>
2023-01-03 03:34:56 +0900

    Auto login and logout button

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -15,6 +15,18 @@ export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "MemoList" }],
+        });
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   function handlePress() {
     firebase
